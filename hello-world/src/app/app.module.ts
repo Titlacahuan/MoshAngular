54eab9f44bba55a115cc3812bf459ae64202ba3d
Titlacahuan <mr.omerali@gmail.com>
2021-03-20 21:47:45 -0400

    Implemented ngOnInit.
    Moved files around to better structure code -- new folders
    (services, pipes, directives folders)

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -1,61 +1,70 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CourseComponent } from './course/course.component';
-import { CoursesComponent } from './courses.component';
-import { CoursesService } from './courses.service';
-import { AuthorsComponent } from './authors/authors.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SummaryPipe } from './summary.pipe';
-import { FavoriteComponent } from './favorite/favorite.component';
-import { TitleCasingPipe } from './titleCasing.pipe';
+
+/* Components */
 import { PanelComponent } from './panel/panel.component';
 import { LikeComponent } from './like/like.component';
-import { InputFormatDirective } from './input-format.directive';
 import { ZippyComponent } from './zippy/zippy.component';
-import { TitleDirective } from './title.directive';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { CourseFormComponent } from './course-form/course-form.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
 import { ChangePasswordFormComponent } from './change-password-form/change-password-form.component';
 import { PostsComponent } from './posts/posts.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
+import { CoursesComponent } from './courses/courses.component';
+
+/* Services */
+import { CoursesService } from './services/courses/courses.service';
+import { PostService } from './services/post/post.service';
+
+/* Directives */
+import { TitleDirective } from './directives/title/title.directive';
+import { InputFormatDirective } from './directives/input-format/input-format.directive';
+
+/* Pipes */
+import { SummaryPipe } from './pipes/summary/summary.pipe';
+import { TitleCasingPipe } from './pipes/titleCasing/titleCasing.pipe';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CourseComponent,
-    CoursesComponent,
-    AuthorsComponent,
-    SummaryPipe,
-    TitleCasingPipe,
-    FavoriteComponent,
-    PanelComponent,
-    LikeComponent,
-    InputFormatDirective,
-    ZippyComponent,
-    TitleDirective,
-    ContactFormComponent,
-    CourseFormComponent,
-	SignupFormComponent,
-	NewCourseFormComponent,
-	ChangePasswordFormComponent,
-	PostsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-	ReactiveFormsModule,
-	HttpClientModule
-  ],
-  providers: [
-    CoursesService
-  ],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		CourseComponent,
+		CoursesComponent,
+		AuthorsComponent,
+		SummaryPipe,
+		TitleCasingPipe,
+		FavoriteComponent,
+		PanelComponent,
+		LikeComponent,
+		InputFormatDirective,
+		ZippyComponent,
+		TitleDirective,
+		ContactFormComponent,
+		CourseFormComponent,
+		SignupFormComponent,
+		NewCourseFormComponent,
+		ChangePasswordFormComponent,
+		PostsComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		FormsModule,
+		ReactiveFormsModule,
+		HttpClientModule
+	],
+	providers: [
+		PostService,
+		CoursesService
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
--- hello-world/src/app/posts/posts.component.ts
+++ hello-world/src/app/posts/posts.component.ts
@@ -1,55 +1,79 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'posts',
-  templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.scss']
+	selector: 'posts',
+  	templateUrl: './posts.component.html',
+  	styleUrls: ['./posts.component.scss']
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
 
 	posts: any[];
 	private url = 'https://jsonplaceholder.typicode.com/posts';
 
-  constructor(private http : HttpClient) { 
-	  http.get(this.url)
-	  	.subscribe((response : any[]) => {
-			  this.posts = response;
-		  });
-  }
+	constructor(private http : HttpClient) { 
+		http.get(this.url)
+			.subscribe((response : any[]) => {
+				this.posts = response;
+		  	});
+	}
 
-  log(x) { 
-	  console.log(x);
-  }
+	ngOnInit(): void {
+		throw new Error('Method not implemented.');
+	}
 
-  createPost(inputTitle : HTMLInputElement) {
-	let post : IPost = {
-		id: undefined,
-		userId: 0,
-		title: inputTitle.value,
-		body: ''
-	};
+/* PREVIOUS LESSON */
+//   log(x) { 
+// 	  console.log(x);
+//   }
 
-	this.http.post(this.url, JSON.stringify(post))
-		.subscribe((response: IPost_PostResponse) => {
-			post.id = response.id;
-			console.log(post);
-			console.log(response);
+//   createPost(inputTitle : HTMLInputElement) {
+// 	let post : IPost = {
+// 		id: undefined,
+// 		userId: 0,
+// 		title: inputTitle.value,
+// 		body: ''
+// 	};
 
-			this.posts.splice(0, 0, post);
+// 	this.http.post(this.url, JSON.stringify(post))
+// 		.subscribe((response: IPost_PostResponse) => {
+// 			post.id = response.id;
+// 			console.log(post);
+// 			console.log(response);
 
-			inputTitle.value = '';
-		});
-  }
+// 			this.posts.splice(0, 0, post);
+
+// 			inputTitle.value = '';
+// 		});
+//   }
+
+//   updatePost(post) {
+// 	  post.title = "Hello Omer";
+// 	  this.http.put(`${this.url}/${post.id}`, JSON.stringify(post))
+// 	  	.subscribe(response => {
+// 			  console.log(response);
+// 			  post = response;
+// 		  });
+//   }
+// deletePost(post) {
+// 	this.http.delete(`${this.url}/${post.id}`)
+// 		.subscribe(response => {
+// 			console.log(response);
+// 			let index = this.posts.indexOf(post);
+// 			this.posts.splice(index, 1);
+// 		});
+// 	}
+// }
+
+// interface IPost {
+// 	id;
+// 	userId;
+// 	title;
+// 	body;
+// }
+
 }
 
-interface IPost {
-	id;
-	userId;
-	title;
-	body;
-}
-
-interface IPost_PostResponse {
-	id;
-}
+// interface IPost_PostResponse {
+// 	id;
+// }
