67faeef76a72f83222365caf95066852b5722cb7
Omer Ali <mr.omerali@gmail.com>
2021-04-02 21:37:21 -0400

    Multiple observables
    Learning how to deal with multiple observables (route parameters and query parameters)

--- hello-world/src/app/followers/followers.component.ts
+++ hello-world/src/app/followers/followers.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { FollowersService } from '../services/followers/followers.service';
+import { combineLatest, Observable } from 'rxjs';
 
 @Component({
   selector: 'followers',
@@ -10,15 +12,30 @@ export class FollowersComponent implements OnInit {
 
 	followers: any[];
 
-	constructor(private service: FollowersService) { }
+	constructor(private route: ActivatedRoute, private service: FollowersService) { }
 
 	ngOnInit(): void {
-		this.service.getAll()
-		.subscribe(
-			(followers: any[]) => {
-				this.followers = followers;
-			}
-		)
+		let obs = combineLatest([
+			this.route.paramMap,
+			this.route.queryParamMap
+		]).subscribe( (combined) => {
+			let page: number = parseInt(combined[1].get('page'));
+			let order: string = combined[1].get('order');
+
+			this.service.getAll()
+			.subscribe(
+				(followers: any[]) => {
+					this.followers = followers;
+				}
+			);
+		});
+
+		// this.route.queryParamMap.subscribe( (params) => {
+		// 	let page: number = parseInt(params.get('page'));
+		// 	let order: string = params.get('order');
+		// });
+
+		
 	}
 
 }
